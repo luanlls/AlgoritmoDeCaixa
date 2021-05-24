4d299ffacb3a9bb9ac3ec6265e734bdf3fa9362f
Luan Lima <83625511+luanlls@users.noreply.github.com>
2021-05-24 19:13:53 -0300

    Update Caixa.java

--- Caixa.java
+++ Caixa.java
@@ -6,120 +6,133 @@ public class Caixa {
 
     public static void main(String[] args) {
 
-        long cpfv, cpf = 12345678900L;
-        int senhav, senha = 01020304;
-        int menu = 0, menuf;
-        float saldo = 1000.00f;
-        int contador = 0, contador2 = 0;
-        int tudo = 0;
-        float deposito, saque;
+        long cpf; //123L
+        int senha; //123
+        double deposito, saque, saldo = 1000.00;
+        int caixa = 0, menu, menuf;
+        int contador;
 
         Scanner ler = new Scanner(System.in);
 
-        while (tudo != 1) {
+        while (caixa == 0) {
 
+            //CPF ----------------------------------------------
+            
             System.out.print("Insira seu cpf: ");
-            cpfv = ler.nextLong();
+            cpf = ler.nextLong();
+            System.out.println("");
 
-            if (cpf != cpfv) {
-
-                System.out.println("");
+            if (cpf != 123) {
+                
                 System.out.println("TELA DE ACESSO NAO PERMITIDA!");
                 System.exit(0);
 
             } else {
+                
+                //SENHA ----------------------------------------------
+                
+                contador = 0;
+                
+                while (contador <= 2) {
 
-                System.out.print("Digite sua senha: ");
-                senhav = ler.nextInt();
-
-                    do {                          
-                        contador++;
-                        System.out.println("");
-                        System.out.println( (3 - contador) + " TENTATIVA(S) RESTANTES!");
-                        System.out.print("Digite sua senha: ");
-                        senhav = ler.nextInt();
-                        
-                        if (contador == 2) {
-                            System.out.println("");
-                            System.out.println("CONTA BLOQUEADA");
-                            System.exit(0);
-                        }
-                        
-                    } while (senha != senhav);
-
-                while (menu < 0 || menu > 3) {
-
+                    contador++;
+                    System.out.print("Insira sua senha: ");
+                    senha = ler.nextInt();
                     System.out.println("");
-                    System.out.println("--------MENU--------");
-                    System.out.println("Aperte 1 para: Saldo.");
-                    System.out.println("Aperte 2 para: Deposito.");
-                    System.out.println("Aperte 3 para: Saque.");
-                    System.out.println("Aperte 0 para: Sair.");
-                    System.out.print("Escolha: ");
-                    menu = ler.nextInt();
 
-                    switch (menu) {
+                    if (senha != 123) {
+                        System.out.println("VOCE TEM: " + (3 - contador) + " TENTATIVAS(S) ");
+                    } else {
 
-                        case 1:
-                            System.out.println("");
-                            System.out.println("Seu saldo é de: " + saldo);
-                            break;
+                        contador = 0;
+                        
+                        // MENU ----------------------------------------------
+                        
+                        for (int i = 0; i < 10; i++) {
+                            
+                            System.out.println("--------MENU--------");
+                            System.out.println("Aperte 1 para: Saldo.");
+                            System.out.println("Aperte 2 para: Deposito.");
+                            System.out.println("Aperte 3 para: Saque.");
+                            System.out.println("Aperte 0 para: Sair.");
+                            System.out.print("Escolha: ");
+                            menu = ler.nextInt();
 
-                        case 2:
-                            System.out.println("");
-                            System.out.println("Insira o valor do deposito: ");
-                            deposito = ler.nextFloat();
-                            System.out.println("");
-                            System.out.println("Seu saldo após o deposito é de: " + (saldo + deposito));
-                            saldo = saldo + deposito;
-                            break;
+                            switch (menu) {
 
-                        case 3:
-                            System.out.println("");
-                            System.out.println("Insira o valor do saque: ");
-                            saque = ler.nextFloat();
-                            System.out.println("");
-                            System.out.println("Seu saldo após o saque é de: " + (saldo - saque));
-                            saldo = saldo - saque;
-                            break;
+                                case 1:
+                                    System.out.println("");
+                                    System.out.println("Seu saldo é de: " + saldo);
+                                    break;
 
-                        case 0:
-                            tudo = 1;
-                            break;
+                                case 2:
+                                    System.out.println("");
+                                    System.out.println("Insira o valor do deposito: ");
+                                    deposito = ler.nextFloat();
+                                    System.out.println("");
+                                    System.out.println("Seu saldo após o deposito é de: " + (saldo + deposito));
+                                    saldo = saldo + deposito;
+                                    break;
 
-                        default:
-                            System.out.println("");
-                            System.out.println("OPERAÇÃO INVALIDA");
-                    }
+                                case 3:
+                                    System.out.println("");
+                                    System.out.println("Insira o valor do saque: ");
+                                    saque = ler.nextFloat();
+                                    System.out.println("");
+                                    System.out.println("Seu saldo após o saque é de: " + (saldo - saque));
+                                    saldo = saldo - saque;
+                                    break;
 
-                    for (int l = 0; l < 10; l++) {
+                                case 0:
+                                    i = 0;
+                                    break;
 
-                        System.out.println("");
-                        System.out.println("Aperte 1 para: Nova operação.");
-                        System.out.println("Aperte 0 para: Sair.");
-                        System.out.print("Escolha: ");
-                        menuf = ler.nextInt();
-                        System.out.println("");
+                                default:
+                                    System.out.println("");
+                                    System.out.println("OPERAÇÃO INVALIDA");
+                                    System.out.println("");
+                                    continue; 
+                            }
+                            
+                            // MENU FINAL --------------------------------------
+                            
+                            do {
+                                
+                                System.out.println("");
+                                System.out.println("Aperte 1 para: Nova operação");
+                                System.out.println("Aperte 0 para: Sair");
+                                System.out.print("Escolha: ");
+                                menuf = ler.nextInt();
+                                System.out.println("");
 
-                        switch (menuf) {
+                                if (menuf != 1 && menuf != 0) {
+                                    System.out.println("");
+                                    System.out.println("OPÇÃO INEXISTENTE");
+                                    i = 10;
+                                    
+                                } else {
+                                    if (menuf == 0){
+                                        i = 10;
+                                        contador = 4;
+                                        
+                                    } else {
+                                        i = 0;
+                                    }
+                                }
 
-                            case 1:
-                                h = 0;
-                                l = 10;
-                                break;
-
-                            case 0:
-                                tudo = 0;
-                                h = 10;
-                                l = 10;
-                                break;
-
-                            default:
-                                l = 0;
+                            } while (menuf != 1 && menuf != 0);
                         }
                     }
+
+                    if (contador == 3) {
+                        System.out.println("");
+                        System.out.println("CONTA BLOQUEADA");
+                        System.exit(0);
+                    }
                 }
-                }
+
             }
         }
+
     }
+}
